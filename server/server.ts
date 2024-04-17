ca86061a4e18c7b071ccbba64c5868e82a5c7c57
Nate Nobleza <noblezanathan@gmail.com>
2024-04-17 07:23:34 +0000

    database endpoints

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ import {
   defaultMiddleware,
   errorMiddleware,
 } from './lib/index.js';
+import { ClientRequest } from 'http';
 
 const connectionString =
   process.env.DATABASE_URL ||
@@ -37,30 +38,94 @@ app.get('/api/runs', async (req, res, next) => {
   }
 });
 
-app.get('/api/runs/:runsId', async (req, res, next) => {
+app.get('/api/runs/:runId', async (req, res, next) => {
   try {
-    const { runsId } = req.params;
-    if (!Number.isInteger(+runsId))
-      throw new ClientError(404, 'runsId must be a number');
+    const { runId } = req.params;
+    if (!Number.isInteger(+runId))
+      throw new ClientError(404, 'runId must be a number');
     const sql = `
     select *
     "runs"
-    where "runsId" = $1
+    where "runId" = $1
     `;
-    const params = [runsId as string];
+    const params = [runId as string];
     const result = await db.query(sql, params);
     const [runs] = result.rows;
-    if (!runs) throw new ClientError(404, `Entry ${runsId} not found`);
+    if (!runs) throw new ClientError(404, `Entry ${runId} not found`);
     res.json(runs);
   } catch (err) {
     next(err);
   }
 });
 
-app.post('/api/runs', async (req, res, next)=>{
-  try{
+app.post('/api/runs', async (req, res, next) => {
+  try {
+    const { time, distance, date, weather, userId } = req.body;
+    if (!time || !distance || !date || !weather || !userId)
+      throw new ClientError(404, 'Entry requires all inputs');
+    const sql = `insert into "runs"
+    ("time", "distance", "date", "weather", "userId")
+    values ($1, $2, $3, $4, $5)
+    returning *`;
+    const params = [
+      time as string,
+      distance as string,
+      date as string,
+      weather as string,
+      userId as string
+    ];
+    const result = await db.query(sql, params);
+    const [entry] = result.rows;
+    if (!entry) throw new ClientError(404, 'entry not found');
+    res.status(201).json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
 
-  } catch(err){
+app.put('/api/runs/:runId', async (req, res, next) => {
+  try {
+    const { runId } = req.params;
+    if (!Number.isInteger(+runId))
+      throw new ClientError(400, 'runId must be a number');
+    const { time, distance, date, weather } = req.body;
+    if (!time || !distance || !date || !weather)
+      throw new ClientError(400, 'time, distance, date, weather needed');
+    const sql = `
+    update "runs"
+    set "time" = $1, "distance" = $2, "date" = $3, "weather" = $4
+    where "runId" = $5
+    returning *
+    `;
+    const params = [
+      time as string,
+      distance as string,
+      date as string,
+      weather as string,
+    ];
+    const result = await db.query(sql, params);
+    const [updatedEntry] = result.rows;
+    if (!updatedEntry) throw new ClientError(404, 'updated entry not found');
+    res.status(200).json(updatedEntry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.delete('/api/runs/:runId', async(req, res, next)=>{
+  try{
+    const { runId } = req.params;
+    if(!Number.isInteger(+runId)) throw new ClientError(400, 'runId not number')
+    const sql = `
+    delete from "runs"
+    where "runId" = $1
+    returning *
+    `;
+    const params = [runId as string]
+    const result = await db.query(sql, params)
+    const [deletedEntry] = result.rows
+    if(!deletedEntry) throw new ClientError(400, 'deleted entry not found')
+  }catch(err){
     next(err)
   }
 })
