73d30169e106f9c16aa88dd9a8064d2346ce309d
Nate Nobleza <noblezanathan@gmail.com>
2024-04-22 23:46:12 +0000

    data.ts halfway done

--- client/src/data.ts
+++ client/src/data.ts
@@ -1,45 +1,44 @@
 type Entry = {
-  runId?: number,
-  date: string,
-  distance: string,
-  time: string,
-  weather: string
-}
+  runId?: number;
+  date: string;
+  distance: string;
+  time: string;
+  weather: string;
+};
 
-async function readEntries(){
-  try{
-    const res = await fetch('/api/runs')
-    if(!res.ok) throw new Error('response connection not okay')
+async function readEntries() {
+  try {
+    const res = await fetch('/api/runs');
+    if (!res.ok) throw new Error('response connection not okay');
     const result = await res.json();
-    return result
-  } catch (err){
-    console.log(err)
+    return result;
+  } catch (err) {
+    console.log(err);
   }
 }
 
 async function readEntry(runId: number) {
-try {
-const res = await fetch('/api/runs/:runId')
-if(!res.ok) throw new Error('response connection not okay')
-const result = await res.json();
-return result
-} catch(err){
-  console.log(err)
-}
+  try {
+    const res = await fetch('/api/runs/:runId');
+    if (!res.ok) throw new Error('response connection not okay');
+    const result = await res.json();
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
-async function addEntry(entry: Entry){
-  try{
-    const res = await fetch('/api/runs',{
+async function addEntry(entry: Entry) {
+  try {
+    const res = await fetch('/api/runs', {
       method: 'POST',
-      headers:{
+      headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(entry)
+      body: JSON.stringify(entry),
     });
-    if(!res.ok) throw new Error('Response connection not OK')
-  }catch(err){
-    console.log(err)
+    if (!res.ok) throw new Error('Response connection not OK');
+  } catch (err) {
+    console.log(err);
   }
 }
